2b49276ccd84af1289e52ac5a52bf2d195076d49
pompos02 <yiannis.caravellas@gmail.com>
2025-08-09 13:48:08 +0300

    lsps sigirismataki

---
+++ .config/nvim/lua/plugins/lsp/basedpyright.lua
@@ -0,0 +1,31 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      basedpyright = {
+        settings = {
+          basedpyright = {
+            -- Disable all formatting and linting (handled by ruff via none-ls)
+            disableOrganizeImports = true,
+            disableLanguageServices = false,
+            analysis = {
+              -- Type checking settings
+              typeCheckingMode = "basic",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              autoImportCompletions = true,
+              diagnosticMode = "openFilesOnly",
+              -- Disable linting features (handled by ruff)
+              diagnosticSeverityOverrides = {
+                reportUnusedImport = "none",
+                reportUnusedVariable = "none",
+                reportUnusedClass = "none",
+                reportUnusedFunction = "none",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
