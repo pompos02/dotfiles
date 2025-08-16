59516d36561c4d621e5d5fe7e764944c288d9bc5
pompos02 <yiannis.caravellas@gmail.com>
2025-08-16 11:46:47 +0300

    tmux visual and minor nvim

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -16,31 +16,16 @@ return {
  ███████████ ███    ███ █████████ █████ █████ ████ █████ 
 ██████  █████████████████████ ████ █████ █████ ████ ██████
                     ]],
-                    -- Simple keybindings
-                    keys = {
-                        {
-                            icon = "",
-                            key = "f",
-                            desc = "Find File",
-                            action = ":lua Snacks.dashboard.pick('files')",
-                        },
-                        {
-                            icon = "",
-                            key = "r",
-                            desc = "Recent Files",
-                            action = ":lua Snacks.dashboard.pick('oldfiles')",
-                        },
-                        {
-                            icon = "",
-                            key = "g",
-                            desc = "Find Text",
-                            action = ":lua Snacks.dashboard.pick('live_grep')",
-                        },
-                    },
                 },
                 -- Minimal sections
                 sections = {
                     { section = "header" },
+                    { section = "terminal",
+                        cmd = "echo 'NVIM v" .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch .. "'",
+                        height = 1,
+                        padding = 1,
+                        align = "center"
+                    },
                     -- { section = "keys", gap = 1, padding = 1 },
                 },
             },
