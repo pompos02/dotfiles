8321dc7f7f52eb995d9362b23bb1bfc439182511
pompos02 <yiannis.caravellas@gmail.com>
2025-10-13 11:30:17 +0300

    style: lualine min

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -284,8 +284,8 @@ return {
                     },
                 },
                 lualine_y = {
-                    { "progress", padding = { left = 1, right = 0 } },
-                    { "location", padding = { left = 0, right = 1 } },
+                    { "progress", padding = { left = 1, right = 1 } },
+                    -- { "location", padding = { left = 0, right = 1 } },
                 },
                 lualine_z = {
                     function()
