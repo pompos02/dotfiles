3e7158fff72de712232d7e138a115eb0dca36e3a
pompos02 <yiannis.caravellas@gmail.com>
2025-11-22 22:20:10 +0200

    good

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,6 +19,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking text",
     group = vim.api.nvim_create_augroup("highlight yank", { clear = true }),
     callback = function()
-        vim.hl.on_yank({ higroup = "YankHighlight", timeout = 150 })
+        vim.highlight.on_yank()
     end,
 })
