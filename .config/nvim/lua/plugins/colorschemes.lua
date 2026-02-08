547d1bc103c2069897a72da7d7428862dc7f5a9a
pompos02 <yiannis.caravellas@gmail.com>
2026-02-09 00:31:09 +0200

    good

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,11 +1,12 @@
 return {
     "vague2k/vague.nvim",
-    -- "pompos02/misirlou.nvim",
+    "pompos02/misirlou.nvim",
     "polirritmico/monokai-nightasty.nvim",
     "loctvl842/monokai-pro.nvim",
     "pompos02/modus-themes.nvim",
     "folke/tokyonight.nvim",
     "craftzdog/solarized-osaka.nvim",
+    "pompos02/yara.nvim",
     "navarasu/onedark.nvim",
     "rebelot/kanagawa.nvim",
     "scottmckendry/cyberdream.nvim",
