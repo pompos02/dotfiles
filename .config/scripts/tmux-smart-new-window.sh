32d146d3987b079ddb25cbdc76fab6ee5f35d047
pompos02 <yiannis.caravellas@gmail.com>
2026-03-08 11:54:53 +0200

    good

--- .config/scripts/tmux-smart-new-window.sh
+++ .config/scripts/tmux-smart-new-window.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo "DEBUG: cmd='$1' pid='$2' path='$3'" >> /tmp/tmux_debug.log
 pane_cmd="$1"
 pane_pid="$2"
 pane_current_path="$3"
@@ -12,7 +11,6 @@ fi
 # We are in SSH Logic
 hostname=$(~/.config/scripts/tmux-ssh-host.sh "$pane_pid" "$pane_cmd")
 host="${hostname#@}"
-echo $host
 
 if [ -n "$host" ]; then
     tmux new-window -c "$pane_current_path" ssh "$host"
