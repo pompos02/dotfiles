668194868c706c743f1ef9496773123d7456269c
pompos02 <yiannis.caravellas@gmail.com>
2026-03-09 20:59:47 +0200

    tmux_sessions

--- .config/scripts/tmux_sessions.sh
+++ .config/scripts/tmux_sessions.sh
@@ -42,7 +42,7 @@ main() {
 				shopt -u nullglob
 			done
 		} |
-		awk -v have_sessions="${#sessions[@]}" '
+			awk -v have_sessions="${#sessions[@]}" '
 			BEGIN { FS="/"; OFS="/" }
 			# First, read all session lines that end with "*"
 			/\*$/ {
@@ -68,40 +68,40 @@ main() {
 	local selected
 	selected="$(
 		case "$THEME" in
-			dark)
-				fzf --border=rounded --info=right \
-					--color=hl:#A5D6FF:reverse:bold,hl+:#79C0FF:reverse:bold \
-					--color=info:white \
-					--color=fg+:#FFFFFF \
-					--color=bg+:#404040
-				;;
-			*)
-				fzf --border=rounded --info=right \
-					--color=fg:#000000,bg:#FFFFFF \
-					--color=hl:#A5D6FF:reverse:bold,hl+:#79C0FF:reverse:bold \
-					--color=info:#000000,separator:#000000,scrollbar:#000000 \
-					--color=fg+:#000000 \
-					--color=bg+:#F2F2F2
-				;;
+		dark)
+			fzf --border=rounded --info=right \
+				--color=hl:#A5D6FF:reverse:bold,hl+:#79C0FF:reverse:bold \
+				--color=info:white \
+				--color=fg+:#FFFFFF \
+				--color=bg+:#404040
+			;;
+		*)
+			fzf --border=rounded --info=right \
+				--color=fg:#000000,bg:#FFFFFF \
+				--color=hl:#A5D6FF:reverse:bold,hl+:#79C0FF:reverse:bold \
+				--color=info:#000000,separator:#000000,scrollbar:#000000 \
+				--color=fg+:#000000 \
+				--color=bg+:#F2F2F2
+			;;
 		esac <<<"$final_list"
-		)" || exit 0  # fzf returns nonzero on cancel
+	)" || exit 0 # fzf returns nonzero on cancel
 
-		[[ -n "$selected" ]] || exit 0
+	[[ -n "$selected" ]] || exit 0
 
-		# Selection handling
-		if [[ "$selected" == *\* ]]; then
-			tmux switch-client -t "${selected%\*}"
-			exit 0
-		fi
+	# Selection handling
+	if [[ "$selected" == *\* ]]; then
+		tmux switch-client -t "=${selected%\*}"
+		exit 0
+	fi
 
-		local dir name
-		dir="$selected"
-		name="$(basename "$dir")"
-		name="${name//./_}"
+	local dir name
+	dir="$selected"
+	name="$(basename "$dir")"
+	name="${name//./_}"
 
-		# Create session only if missing; then switch.
-		tmux has-session -t "$name" 2>/dev/null || tmux new-session -d -s "$name" -c "$dir"
-		tmux switch-client -t "$name"
-	}
+	# Create session only if missing; then switch.
+	tmux has-session -t "=$name" 2>/dev/null || tmux new-session -d -s "$name" -c "$dir"
+	tmux switch-client -t "=$name"
+}
 
 main "$@"
