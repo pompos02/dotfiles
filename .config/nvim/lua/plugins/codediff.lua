4116dfc16cd1dc0bcdaef6a7f490c178d9fd2e1d
pompos02 <yiannis.caravellas@gmail.com>
2026-04-27 20:13:46 +0300

    good

--- .config/nvim/lua/plugins/codediff.lua
+++ .config/nvim/lua/plugins/codediff.lua
@@ -1,4 +1,14 @@
+-- Packer
 return {
+    -- "esmuellert/codediff.nvim",
     dir = "/home/karavellas/projects/opensource/codediff.nvim",
     cmd = "CodeDiff",
+    opts = {
+        highlights = {
+            char_brightness = nil,
+        },
+        diff = {
+            ignore_trim_whitespace = false, -- Ignore leading/trailing whitespace changes (like diffopt+=iwhite)
+        },
+    },
 }
