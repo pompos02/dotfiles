060f23321fea806da8d377ba8a5fdbd00f978aba
pompos02 <yiannis.caravellas@gmail.com>
2026-06-17 23:00:56 +0300

    good

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,7 +11,7 @@ vim.g.plsql_fold = 1
 
 vim.opt.background = "dark"
 
-vim.cmd.colorscheme("dolores-mitropanos")
+vim.cmd.colorscheme("dolores-thes")
 
 vim.o.winborder = "single"
 
--- .config/nvim/lua/plugins/indent.lua
+++ .config/nvim/lua/plugins/indent.lua
@@ -1,38 +1,42 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    event = { "BufReadPost", "BufNewFile" },
+    "folke/snacks.nvim",
     opts = {
         indent = {
-            char = "┊",
-        },
-        scope = {
-            enabled = true,
-            show_start = false,
-            show_end = false,
-            char = "│",
-            include = {
-                node_type = {
-                    ["*"] = {
-                        "return_statement",
-                        "function_call",
-                        "for_statement",
-                        "if_statement",
-                        "variable_assignment",
-                        "declaration",
-                    },
-                    lua = {
-                        "field",
-                    },
-                    rust = {
-                        "let_declaration",
-                        "call_expression",
-                        "arguments",
-                        -- "field_expression",
-                        "closure_expression",
-                    },
+            indent = {
+                enabled = true,
+                char = "┊",
+                only_scope = false,
+                only_current = false,
+                hl = "SnacksIndent",
+            },
+            animate = {
+                enabled = vim.fn.has("nvim-0.10") == 1,
+                style = "out",
+                easing = "linear",
+                duration = {
+                    step = 20,
+                    total = 500,
                 },
             },
+
+            scope = {
+                enabled = true,
+                priority = 200,
+                char = "│",
+                underline = false,
+                only_current = false,
+                hl = "SnacksIndentScope",
+            },
+
+            chunk = {
+                enabled = false,
+            },
+
+            filter = function(buf)
+                return vim.g.snacks_indent ~= false
+                    and vim.b[buf].snacks_indent ~= false
+                    and vim.bo[buf].buftype == ""
+            end,
         },
     },
 }
