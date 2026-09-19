2ce32f96f40790f63975a5f19d3d512f596a7fdd
pompos02 <yiannis.caravellas@gmail.com>
2026-09-19 15:13:13 +0300

    chore: nvim configuration touchup - New todo plugin logic

--- home/.config/nvim/lua/plugins/todo-comments.lua
+++ home/.config/nvim/lua/plugins/todo-comments.lua
@@ -3,9 +3,9 @@ return {
     opts = {
         highlighters = {
             fixme     = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-            hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
-            todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-            note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+            hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsFixme' },
+            todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsFixme' },
+            note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsFixme' },
         },
     },
 }
